eda8f6326a05756614180d51c80a6e3a44d7cd52
Lwarentina <raditya.hanarta@gmail.com>
2024-01-16 14:06:36 +0700

    seg

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ class App extends Component {
       type: "danger",  
       header: "Fatal Error",  
       content: "ini content dari alert"  
-    }  
+    } 
   }
-      // fungsi untuk mengubah tipe  
+    // fungsi untuk mengubah tipe  
   changeTypeAlert = (event) =>{  
     this.setState({type: event.target.value});  
   }  
   // fungsi untuk mengubah header  
-  changeHeaderAlert = (event) => {   
+  changeHeaderAlert = (event) => {  
     this.setState({header: event.target.value});  
   }  
   // fungsi untuk mengubah content  
@@ -35,7 +35,7 @@ class App extends Component {
             <button className="mr-1 btn btn-success">Setuju</button>  
             <button className="btn btn-info">Iya Dong</button>  
           </div>  
-        </div>
+        </div>  
         <div className='container'>
               <Media image="save.png" title="React Js" type="info">
                   Framework untuk FrontEnd Development
@@ -43,6 +43,29 @@ class App extends Component {
               <Media image="owi-removed.png" title="Node Js" type="success">
                   Framework untuk BackEnd Development
               </Media>
+        </div>
+        <div className="App container col-sm-6">  
+          <Alert type={this.state.type} header={this.state.header}>  
+            {this.state.content}  
+          </Alert>
+          <hr />  
+          <h4>Alert Control</h4>  
+          <b className="text-left">Tipe Alert</b>  
+          <select className="form-control" name="type" value={this.state.type}  
+            onChange={this.changeTypeAlert}>  
+            <option value="success">Success</option>  
+            <option value="warning">Warning</option>  
+            <option value="danger">Danger</option>  
+            <option value="info">Info</option>  
+          </select>  
+          <b className="text-left">Header Alert</b>  
+          <input  
+            type="text" name="header" className="form-control" value={this.state.header}  
+            onChange={this.changeHeaderAlert} />  
+          <b className="text-left">Content Alert</b>  
+          <input  
+            type="text" name="content" className="form-control" value={this.state.content}  
+            onChange={this.changeContentAlert} />
         </div>
       </div>
     );
